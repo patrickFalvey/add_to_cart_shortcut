69b10e17b23a3f82ff21d6e44cebb69f658a7162
patrickFalvey <patrickfalvey40@gmail.com>
2015-07-14 21:13:33 -0700

    Add different test

---
+++ davidson_mechanize_test.py
@@ -0,0 +1,65 @@
+import mechanize
+
+
+upc = '723364202001'
+
+def add_to_cart_davidson(upc):
+
+
+    username = 'scott@gsadirect.com'
+    password = 'gsadirect1'
+    
+    br=mechanize.Browser()
+    br.set_handle_robots(False)
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+    br.addheaders = [
+        ('User-agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36')
+        ]
+    url = "http://www11.davidsonsinc.com/Login/Login.aspx"
+    br.open(url)
+
+
+    for form in br.forms():
+        print "Form name:", form.name
+        print form
+##    br.select_form(name="aspnetForm")
+
+
+    br.select_form(nr=0)
+    br.form['ctl00$ContentPlaceHolderNavPane$NavLoginForm$UserLogin$UserName'] = username
+    br.form['ctl00$ContentPlaceHolderNavPane$NavLoginForm$UserLogin$Password'] = password
+    br.submit()
+
+
+
+
+
+##    br.select_form(nr=0)
+##    br.form['upc']= upc
+##    br.submit()
+##    print br.geturl()
+
+
+##    content = req.get_data()
+##    print content  
+
+
+        
+    product_page = 'http://www11.davidsonsinc.com/Dealers/ItemDetail.aspx?sid=%s&scode=upcID' % (upc,)
+    br.open(product_page)
+
+    for form in br.forms():
+        print "Form name:", form.name
+        print form
+
+    print br.geturl()
+
+
+
+add_to_cart_davidson(upc)
+
+
+
+
+
+
